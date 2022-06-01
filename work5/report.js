cbbddb5bfb85006ef7d56f1e5ade1db1045ee46e
so0y <jenny1717@naver.com>
2022-06-01 22:10:34 +0900

    frontStudy2

--- work5/report.js
+++ work5/report.js
@@ -5,16 +5,17 @@ const newNumbers = [];
 // numbers 배열을 기본 반복문 (for,while)을 통해 중복인 숫자를 제거하여 새로운 배열로 만들기 ! 
 // ( 제한 : 내장 함수 사용 금지 ! )
 for(let i = 0; i < numbers.length; i++){
+	let isCheck = true;
 	for(let j = 0; j < newNumbers.length; j++){
 		if(numbers[i] === newNumbers[j]){
-			newNumbers.push(numbers[i]);
+			isCheck = false;
 		}
-		console.log(newNumbers);
+	}
+	if(isCheck){
+		newNumbers[newNumbers.length] = numbers[i]
 	}
 }
-
-
-
+console.log(newNumbers);
 // --------------------------------------------------------------------
 
 // 과제 2번
@@ -34,11 +35,10 @@ console.log(cnt);
 // Math.random( ), Math.floor( 값 )함수를 이용하여 중복없는 숫자 10개를 새로운 배열 만들기 !
 // : Math.random( )은 0.0~0.999...의 수를 램덤으로 하나 부여하는 함수 !
 // : Math.floor(  )은 (  )안에 값을 실수의 값을 넣으면 실수의 최대 정수를 반환하는 함수 !
-
 const arr = [];
 for(let i = 0; i < 10; i++){
 	rnd = Math.floor(Math.random()*100);
-	if(rnd !== arr[i]){
+	if(!arr.includes(rnd)){
 		arr.push(rnd);
 	}
 }
@@ -75,7 +75,6 @@ bookCafe_arr.forEach((e) => {
 
 // 과제 6번 
 // findIndex 함수로 _obj의 가격이 2000인 메뉴 찾고 index를 이용하여 메뉴 이름 출력하기 !
-
 const price = bookCafe_obj.findIndex((e) => e.price === 2000);
 console.log(bookCafe_obj[price].name);
 
@@ -84,12 +83,13 @@ console.log(bookCafe_obj[price].name);
 // 과제 7번 
 // filter 함수로 _obj 북카페 메뉴 2700 인 것 분류하기 !
 const menu = bookCafe_obj.filter((e) => e.price === 2700);
-console.log(bookCafe_obj[menu].name);
+console.log(menu);
 //----------------------------------------------------------------------
 
 // 과제 8번 
-// sort 함수로 _obj 북카페 메뉴 낮은 가격 기준으로 정렬하기 !  
-const low = bookCafe_obj.sort(function (a, b){
+// sort 함수로 _obj 북카페 메뉴 낮은 가격 기준으로 정렬하기 !
+let low = [...bookCafe_obj];
+low.sort(function (a, b){
 	return a.price - b.price;
 });
 console.log(low);
@@ -97,7 +97,8 @@ console.log(low);
 
 // 과제 9번
 // sort 함수로 _obj 북카페 메뉴 이름을 사전식으로 정렬하기 !
-const menuSort = bookCafe_obj.sort(function (a, b){
+let menuSort = [...bookCafe_obj];
+menuSort.sort(function (a, b){
 	return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
 });
 console.log(menuSort);
