bdfa8020294b1fc40241f56e424557c130bff6ef
freemoon99 <soloist132@gmail.com>
2022-06-30 02:28:27 +0900

    study chapter2

---
+++ 리액트/study-react/src/styles.js
@@ -0,0 +1,19 @@
+import { whileStatement } from '@babel/types';
+import { createGlobalStyle } from 'styled-components';
+
+//yarn add styled-reset
+import reset from 'styled-reset';
+
+export const lightTheme = {
+    fontColor: '#2c2c2c',
+    bgColor: 'white',
+};
+
+export const darkTheme = {
+    fontColor: 'white',
+    bgColor: '#2c2c2c', 
+};
+
+export const GlobalStyles = createGlobalStyle`
+    ${reset}
+`;
