b2ae4dead8457480c3a18327aa6ef4b11c783a54
Miniling <bsm3925@naver.com>
2022-07-09 20:34:09 +0900

    final

--- Frontend-study-report_#4/myreact/src/App.js
+++ Frontend-study-report_#4/myreact/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/header';
+import { Routes, Route } from 'react-router-dom';
+import Home from './page/home';
+import About from './page/about';
+import People from './page/people';
+import Info from './component/info';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <section id='header'>
+        <Header />
+      </section>
+
+      <section id='body'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/people' element={<People />}>
+            <Route path=':department' element={<Info />} />
+          </Route>
+        </Routes>
+      </section>
     </div>
   );
 }
