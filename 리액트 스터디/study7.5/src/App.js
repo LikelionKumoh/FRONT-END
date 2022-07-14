6c4ad896731d99d774bf7770886fe669d9d1a1fb
freemoon99 <soloist132@gmail.com>
2022-07-14 22:04:56 +0900

    finish?

--- 리액트 스터디/study7.5/src/App.js
+++ 리액트 스터디/study7.5/src/App.js
@@ -4,7 +4,6 @@ import Home from './pages/home';
 import About from './pages/about';
 import Peoples from './pages/peoples';
 import Menubar from './pages/menubar';
-import People from './pages/people';
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="/about" element={<About />}/>
         <Route path="/people" element={<Peoples />} />
-          <Route path="people/:id" element={<People />} />
       </Route>
     </Routes>
   );
--- 리액트 스터디/study7.5/src/pages/peoples.jsx
+++ 리액트 스터디/study7.5/src/pages/peoples.jsx
@@ -1,20 +1,57 @@
+import { useState } from 'react';
 import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
+import { getFrontEndInfo } from '../data/FrontEndInfo';
 
 const Peoples = () => {
+
+    function getDepHandler(e)
+    {
+        const ListTmp = frontEndInfo.filter((element)=>{if(e.target.id === element.department) return element});
+        setNameList(ListTmp);
+    }
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const detail = searchParams.get("detail")
+
+
+    const handleClick = () => {
+        setSearchParams({ detail: detail === "true" ? false: true})
+    }
+    
+    let frontEndInfo = getFrontEndInfo();
+    const [NameList,setNameList] = useState([]);
+
     return (
         <div>
             <header>
                 <h1 style={{color:'#08088A', textAlign:'center'}}>KIT LIKELION FRONT-END</h1>
             </header>
-            <main>
-                <ul style={{display:'flex', flexDirection:'row', textAlign:'center', justifyContent:'space-evenly', width:'70%', margin:'auto'}}>
-                    <li style={{listStyle: 'none'}}><NavLink to='/people/1'>컴퓨터공학과</NavLink></li>
-                    <li style={{listStyle: 'none'}}><NavLink to='/people/2'>컴퓨터소프트웨어공학과</NavLink></li>
-                    <li style={{listStyle: 'none'}}><NavLink to='/people/3'>메디컬IT융합공학과</NavLink></li>
-                    <li style={{listStyle: 'none'}}><NavLink to='/people/4'>전자IT융합공학과</NavLink></li>
-                </ul>
-                <Outlet/>
+            <main style={{width:'60%', margin:'auto', padding:'0px'}}>
+                <ul style={{display:'flex', justifyContent:'space-between'}}>
+                    <li id="컴퓨터공학과" style={{listStyle: 'none'}} onClick={getDepHandler}>컴퓨터공학과</li>
+                    <li id="컴퓨터소프트웨어공학과" style={{listStyle: 'none'}} onClick={getDepHandler}>컴퓨터소프트웨어공학과</li>
+                    <li id="메디컬IT융합" style={{listStyle: 'none'}} onClick={getDepHandler}>메디컬IT융합공학과</li>
+                    <li id="전자IT융합" style={{listStyle: 'none'}} onClick={getDepHandler}>전자IT융합공학과</li>
+                </ul>   
+                <div>
+                    {   NameList.map((props)=>{
+                            return (
+                            <div style={{display:'flex', flexDirection:'column',margin:'auto',width:'80%'}}>
+                                <div style={{display:'flex', border:'2px solid', marginBottom:'1%'}}>
+                                    <img src={props.photo}/>
+                                    <p style={{marginTop:'auto', marginBottom:'auto', width:'30%'}}>{props.name}<br/>{props.department}</p>
+                                    <button id='button' onClick={handleClick} style={{width:'20%', marginTop:'17%', marginLeft:'30%',backgroundColor:'orange', color:'white', border:'none', borderRadius:'5px'}}>자세히보기</button>
+                                </div>
+                                <div style={{textAlign:'left', border:'2px solid', marginBottom:'1%'}}>
+                                    {detail === 'true' ? <p>MBTI: {props.MBTI}</p>: ""}
+                                    {detail === 'true' ? <p>INTRODUCE: {props.detail}</p> : ""}
+                                </div>
+                            </div>
+                        )})
+                    }
+                </div>
+                
             </main>
         </div>
     );
