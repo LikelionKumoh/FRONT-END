ae933f689053b0028c3f5174c3e1285ac93b1558
freemoon99 <soloist132@gmail.com>
2022-07-20 09:12:25 +0900

    d

--- 리액트 스터디/study7.5/src/pages/menubar.jsx
+++ 리액트 스터디/study7.5/src/pages/menubar.jsx
@@ -4,19 +4,20 @@ import { NavLink, Outlet } from 'react-router-dom';
 
 const Menubar = () => {
     return (
-        <div style={{textAlign:'center'}}>
+        <div style={{ textAlign: 'center' }}>
             <img src='/img/LIKELION_image.png' />
             <ul style={{
-                    display:'flex', border:'2px solid #0B3B0B', borderLeft:'0px', borderRight:'0px', 
-                    justifyContent:'space-evenly', width:'60%', textDecoration:'none',
-                    margin:'auto', paddingTop:'5px', paddingBottom:'5px'}}>
-                <li style={{listStyle: 'none'}}>
+                display: 'flex', border: '2px solid #0B3B0B', borderLeft: '0px', borderRight: '0px',
+                justifyContent: 'space-evenly', width: '60%', textDecoration: 'none',
+                margin: 'auto', paddingTop: '5px', paddingBottom: '5px'
+            }}>
+                <li style={{ listStyle: 'none' }}>
                     <NavLink to='/'>HOME</NavLink>
                 </li>
-                <li style={{listStyle: 'none'}}>
+                <li style={{ listStyle: 'none' }}>
                     <NavLink to='/about'>ABOUT</NavLink>
                 </li>
-                <li style={{listStyle: 'none'}}>
+                <li style={{ listStyle: 'none' }}>
                     <NavLink to='/people'>PEOPLE</NavLink>
                 </li>
             </ul>
